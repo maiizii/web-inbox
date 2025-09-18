0dd6c275e9f5c56c430986e9c8df89cce3f8ed54
maiizii <59905570+maiizii@users.noreply.github.com>
2025-09-18 02:06:15 -0700

    Create Card.jsx

---
+++ src/components/ui/Card.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { clsx } from "clsx";
+
+export default function Card({ children, className, ...rest }) {
+  return (
+    <div
+      className={clsx(
+        "card p-4 fade-in",
+        className
+      )}
+      {...rest}
+    >
+      {children}
+    </div>
+  );
+}
