a1206645107f828c1f6f40aa07d7d6ec346ad7e6
maiizii <59905570+maiizii@users.noreply.github.com>
2025-09-18 02:03:03 -0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,9 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { AuthProvider, useAuth } from "./context/AuthContext.jsx";
 import AuthPage from "./pages/AuthPage.jsx";
 import InboxPage from "./pages/InboxPage.jsx";
-import "./App.css";
+import Layout from "./components/layout/Layout.jsx";
+import { ThemeProvider } from "./context/ThemeContext.jsx";
+import { ToastProvider } from "./hooks/useToast.js";
 
 function PrivateRoute({ children }) {
   const { user, loading } = useAuth();
@@ -14,21 +16,27 @@ function PrivateRoute({ children }) {
 
 export default function App() {
   return (
-    <AuthProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/auth" element={<AuthPage />} />
-            <Route
-              path="/"
-              element={
-                <PrivateRoute>
-                  <InboxPage />
-                </PrivateRoute>
-              }
-            />
-          <Route path="*" element={<Navigate to="/" replace />} />
-        </Routes>
-      </BrowserRouter>
-    </AuthProvider>
+    <ThemeProvider>
+      <AuthProvider>
+        <ToastProvider>
+          <BrowserRouter>
+            <Routes>
+              <Route path="/auth" element={<AuthPage />} />
+              <Route
+                path="/"
+                element={
+                  <PrivateRoute>
+                    <Layout>
+                      <InboxPage />
+                    </Layout>
+                  </PrivateRoute>
+                }
+              />
+              <Route path="*" element={<Navigate to="/" replace />} />
+            </Routes>
+          </BrowserRouter>
+        </ToastProvider>
+      </AuthProvider>
+    </ThemeProvider>
   );
 }
