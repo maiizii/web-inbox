28471236184e75cf64fd4c3ceff8e9f03ecba9b7
maiizii <59905570+maiizii@users.noreply.github.com>
2025-09-19 19:20:45 -0700

    Update BlockEditorAuto.jsx

--- src/components/blocks/BlockEditorAuto.jsx
+++ src/components/blocks/BlockEditorAuto.jsx
@@ -760,7 +760,7 @@ export default function BlockEditorAuto({
             className={`split-divider ${
               previewMode === "vertical" ? "split-vertical" : "split-horizontal"
             } ${draggingDivider ? "dragging" : ""}`}
-            onMouseDown={console.log('divider down'); startDividerDrag}
+            onMouseDown={e => {console.log('divider down'); startDividerDrag(e);}}
             onTouchStart={startDividerDrag}
             onDoubleClick={resetSplit}
             title="拖动调整比例，双击恢复 50%"
