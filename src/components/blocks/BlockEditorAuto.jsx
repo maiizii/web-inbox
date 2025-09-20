76319b0ecea64611357e31e53e2ec8d6f2a98f27
maiizii <59905570+maiizii@users.noreply.github.com>
2025-09-19 19:12:19 -0700

    Update BlockEditorAuto.jsx

--- src/components/blocks/BlockEditorAuto.jsx
+++ src/components/blocks/BlockEditorAuto.jsx
@@ -468,6 +468,7 @@ export default function BlockEditorAuto({
     window.addEventListener("mouseup", stopDividerDrag);
     window.addEventListener("touchmove", onDividerMove, { passive: false });
     window.addEventListener("touchend", stopDividerDrag);
+    onMouseDown={e => {console.log('divider down'); startDividerDrag(e);}}
   }
   function onDividerMove(e) {
     if (!draggingDivider || !dividerDragRef.current) return;
